408b13df431d254366d24abd1e6e6bec1cc7285f
Viorel Sfetea <viorel.sfetea@gmail.com>
2020-11-22 14:53:21 +0100

    Added tooltips for longer items

--- src/partials/Item.js
+++ src/partials/Item.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Tooltip from "monday-ui-react-core/dist/Tooltip";
 
 class Item extends React.Component {
   constructor(props) {
@@ -17,12 +18,28 @@ class Item extends React.Component {
     });
   }
 
-  render() {
+  getItemRender() {
     return <div className="ItemWrapper">
-    <div className="Item" onClick={() => this.props.onItemClick(this.props.item)} draggable={this.props.draggable ? this.props.draggable : false} onDragStart={this.onDragStart.bind(this)}>
-          <div className="ItemColor" style={{background: (this.props.color)}}></div>
-          <h5>{this.props.item.name}</h5>
-        </div></div>;
+          <div className="Item" onClick={() => this.props.onItemClick(this.props.item)} draggable={this.props.draggable ? this.props.draggable : false} onDragStart={this.onDragStart.bind(this)}>
+            <div className="ItemColor" style={{background: (this.props.color)}}></div>
+            <h5>{this.props.item.name}</h5>
+          </div>
+        </div>
+  }
+
+  render() {
+    if(this.props.item.name.length <= 30) {
+      return this.getItemRender();
+    }
+    
+    return <Tooltip
+        showDelay={300}
+        immediateShowDelay={0}
+        content={this.props.item.name}
+        containerSelector="body"
+      >
+        {this.getItemRender()}
+      </Tooltip>;
   }
 }
 
