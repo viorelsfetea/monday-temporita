796db7774e8c160e38e59a85edb554670d27bb7a
Viorel Sfetea <viorel.sfetea@gmail.com>
2021-01-02 21:24:32 +0100

    Oops forgot to stage the logo file

--- src/data/ItemsDao.js
+++ src/data/ItemsDao.js
@@ -84,6 +84,7 @@ class ItemsDao {
   }
 
   includeBoard(board) {
+    console.log('ItemsDao.js:87', board, board.id, this.context, this.context.boardsId);
     return board.name.indexOf("Subitems") === -1 && board.id === this.context.boardId;
   }
 }
